2496fe20b1ee53345560dfcea19a58488295972b
seeda5023 <seeda5023@HRH-IC0037398.wrdsb.ca>
2019-04-29 13:42:21 -0400

    Added System out thing

--- src/grouptest/GroupTest.java
+++ src/grouptest/GroupTest.java
@@ -15,7 +15,11 @@ public class GroupTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("What is uuuup");
+        
+        
+        
     }
     
 }
